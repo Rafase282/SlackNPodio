96f3211548b4cc7a7223cb5410076811f406fb36
Sean Lilley <lilleyse@gmail.com>
2017-02-20 20:55:31 -0500

    Getting podio data

--- bot.js
+++ bot.js
@@ -1,29 +1,40 @@
-var Keys = require('./keys');
-
 var RtmClient = require('@slack/client').RtmClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 
-var bot_token = Keys.botToken();
+var Podio = require('podio-js').api;
 
+var Keys = require('./keys');
+
+var bot_token = Keys.botToken;
 var rtm = new RtmClient(bot_token);
+var podio;
+var podioAuthenticated = false;
 
-var channelId;
+// get the API id/secret
+var clientId = Keys.clientId;
+var clientSecret = Keys.clientSecret;
 
-var podio = require('./podio-js');
+// get the app ID and Token for appAuthentication
+var appId = Keys.appID;
+var appToken = Keys.appToken;
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-    var channels = rtmStartData.channels;
-    for (var i = 0; i < channels.length; i++) {
-        var channel = channels[i];
-        if (channel.name === 'general') {
-            channelId = channel.id;
-        }
-    }
-
     console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+    authenticatePodio(function() {
+        podioAuthenticated = true;
+    }, function(err) {
+        console.log(err);
+    });
 });
+
+function getTitle() {
+    var externalId = 'title';
+    return podio.request('GET', '/app/' + appId + '/field/' + externalId).then(function(responseData) {
+        return responseData.status;
+    })
+}
 
 // // you need to wait for the client to fully connect before you can send messages
 // rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
@@ -32,70 +43,39 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
 
 rtm.on(RTM_EVENTS.MESSAGE, function (message) {
     // TODO : look for /podio command somehow
-    console.log(message);
+    // Looks for "podio status"
     var channelId = message.channel;
     var split = message.text.split(' ');
-    if (split[1] === 'data') {
-        // Call Podio API
-        rtm.sendMessage('Getting podio data', channelId);
+    if (split[1] === 'status') {
+        if (podioAuthenticated) {
+            getTitle().then(function(status) {
+                // Call Podio API
+                rtm.sendMessage('Title status: ' + status, channelId);
+            }).catch(function(err) {
+                console.log(err);
+            });
+        } else {
+            console.log('podio is not authenticated');
+        }
     }
 });
-
 
 rtm.start();
 
 
-
-
-
-
-
-    //Setup the API client
-
-    var podio = new Podio({
-        authType: 'client',
+function authenticatePodio(callback, errorCallback) {
+    // instantiate the SDK
+    podio = new Podio({
+        authType: 'app', // or client
         clientId: clientId,
         clientSecret: clientSecret
-      });
-
-
-    //Authenticate
-    const Podio = require('./podio-js').api;
-
-    // Example config file where you might store your credentials
-    import Keys from '../keys.js'
-
-    // get the API id/secret
-    const clientId = Keys.clientId;
-    const clientSecret = Keys.clientSecret;
-
-    // get the app ID and Token for appAuthentication
-    const appId = Keys.appId;
-    const appToken = Keys.appToken;
-
-    // instantiate the SDK
-    const podio = new Podio({
-      authType: 'app',
-      clientId: clientId,
-      clientSecret: clientSecret
     });
 
-    podio.authenticateWithApp(appId, appToken, (err) => {
-
-      if (err) throw new Error(err);
-
-      podio.isAuthenticated().then(() => {
-        // Ready to make API calls in here...
-
-      }).catch(err => console.log(err));
-
+    return podio.authenticateWithApp(appId, appToken, function(err) {
+        return podio.isAuthenticated().then(function() {
+            callback();
+        }).catch(function(err) {
+            errorCallback(err);
+        });
     });
-
-
-
-    //Make your API calls
-
-    podio.request('GET', '/tasks', null, function(responseData) {
-      // do something with the data
-      console.log(responseData);
-    });
+}
--- keys.js
+++ keys.js
@@ -3,16 +3,8 @@ module.exports = Keys;
 function Keys() {
 }
 
-Keys.botToken = function() {
-    return 'xoxb-143577450210-kVJwZ5RbAlv8BxEtMBwKk5Lf';
-};
-
-Keys.podioAPI = function() {
-    return '';
-}
-
-
+Keys.botToken = 'xoxb-143577450210-kVJwZ5RbAlv8BxEtMBwKk5Lf';
 Keys.clientId = 'slack-z0j36s';
-Keys.clientSecret = 'FXnZUlRKIIsSbC34sYHT4RQxhwym19ifaEeLh2Dxpi7Z5drs6SfWymYKyEJsg4G';
+Keys.clientSecret = '5FXnZUlRKIIsSbC34sYHT4RQxhwym19ifaEeLh2Dxpi7Z5drs6SfWymYKyEJsg4G';
 Keys.appToken = 'b401638e39a745b7b32afbe6f34d9c36';
 Keys.appID = '17912486';
