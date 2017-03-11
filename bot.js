4e4a4aea007e2335a2e177c37c3eb8368dd4ba77
Rafael J. Rodriguez <rafase282@gmail.com>
2017-03-11 12:23:30 -0500

    Clean syntax

--- bot.js
+++ bot.js
@@ -1,84 +1,69 @@
-var RtmClient = require('@slack/client').RtmClient;
-var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-
-var Podio = require('podio-js').api;
-
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+const Podio = require('podio-js').api;
 require('dotenv').config({
   silent: true
 });
-
-var bot_token = process.env.botToken;
-var rtm = new RtmClient(bot_token);
-var podio;
-var podioAuthenticated = false;
-
-// get the API id/secret
-var clientId = process.env.clientId;
-var clientSecret = process.env.clientSecret;
-
-// get the app ID and Token for appAuthentication
-var appId = process.env.appID;
-var appToken = process.env.appToken;
+let podioAuthenticated = false;
+const rtm = new RtmClient(process.env.botToken);
+const podio = new Podio({
+  authType: 'app', // or client
+  clientId: process.env.clientId,
+  clientSecret: process.env.clientSecret
+});
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
-    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-    authenticatePodio(function() {
-        podioAuthenticated = true;
-    }, function(err) {
-        console.log(err);
-    });
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+  authenticatePodio(function() {
+    podioAuthenticated = true;
+  }, function(err) {
+    console.log(err);
+  });
 });
 
 function getTitle() {
-    var externalId = 'title';
-    return podio.request('GET', '/app/' + appId).then(function(responseData) {
-      console.log(responseData);
-        return responseData.status;
-    })
+  let externalId = 'title';
+  return podio.request('GET', '/app/' + process.env.appID).then(function(responseData) {
+    console.log(responseData);
+    return responseData.status;
+  })
 }
 
 // // you need to wait for the client to fully connect before you can send messages
- rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-     rtm.sendMessage("Hello! Just letting you know that I'm here if you need anything.", 'C46S9UAN5');
- });
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+  rtm.sendMessage("Hello! Just letting you know that I'm here if you need anything.", 'C46S9UAN5');
+});
 
-rtm.on(RTM_EVENTS.MESSAGE, function (message) {
-    // TODO : look for /podio command somehow
-    // Looks for "podio status"
-    var channelId = message.channel;
-    var split = message.text.split(' ');
-    if (split[1] === 'status') {
-        if (podioAuthenticated) {
-            getTitle().then(function(status) {
-                // Call Podio API
-                rtm.sendMessage('Title status: ' + status, channelId);
-            }).catch(function(err) {
-                console.log(err);
-            });
-        } else {
-            console.log('podio is not authenticated');
-        }
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+  // TODO : look for /podio command somehow
+  // Looks for "podio status"
+  let channelId = message.channel;
+  let split = message.text.split(' ');
+  if (split[1] === 'status') {
+    if (podioAuthenticated) {
+      getTitle().then(function(status) {
+        // Call Podio API
+        rtm.sendMessage('Title status: ' + status, channelId);
+      }).catch(function(err) {
+        console.log(err);
+      });
+    } else {
+      console.log('podio is not authenticated');
     }
+  }
 });
 
 rtm.start();
 
 
 function authenticatePodio(callback, errorCallback) {
-    // instantiate the SDK
-    podio = new Podio({
-        authType: 'app', // or client
-        clientId: clientId,
-        clientSecret: clientSecret
+  return podio.authenticateWithApp(process.env.appID, process.env.appToken, function(err) {
+    return podio.isAuthenticated().then(function() {
+      callback();
+    }).catch(function(err) {
+      errorCallback(err);
     });
-
-    return podio.authenticateWithApp(appId, appToken, function(err) {
-        return podio.isAuthenticated().then(function() {
-            callback();
-        }).catch(function(err) {
-            errorCallback(err);
-        });
-    });
+  });
 }
