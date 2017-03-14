05f7ed359d0a830e05751ff2d349bc18b123ea31
Rafael J. Rodriguez <rafase282@gmail.com>
2017-03-14 00:54:03 -0400

    Partial implementation of setStatus

--- bot.js
+++ bot.js
@@ -13,11 +13,6 @@ const podio = new Podio({
   clientSecret: process.env.clientSecret
 });
 
-// Adds function to capitalize first letter and lowecase the rest to the string object.
-String.prototype.titleCase = function() {
-  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
-}
-
 // Function to filter array of fields
 function filterFields(fields, key) {
   return fields.filter((field) => field.label === key)
@@ -36,69 +31,81 @@ function filterItems(title, field) {
     'remember': false
   }
   return podio.request('POST', '/item/app/17912486/filter/', data)
-    .then(function(responseData) {
-      return responseData.items[0].fields;
-    });
+    .then((res) => res.items);
 }
 
+function getItemID(item, field) {
+  return filterItems(item, field)
+    .then((res) => res[0].item_id);
+
+}
 // function to get values
 function getStatus(item, field, channel) {
   return filterItems(item, field)
-    .then(function(fields) {
-      let res = filterFields(fields, field)[0].values[0].value.text;
+    .then((items) => {
+      const res = filterFields(items[0].fields, field)[0].values[0].value.text;
       rtm.sendMessage('Item: ' + item + ', Field: ' + field + ', Value(s): ' + res, channel);
     });
 }
 
 //Sets status field to value Active or Inactive
 //Action: @podio set status [value: Active or Inactive]
-function setStatus(itemId, msg, channel) {
-  return podio.request('POST', '/app/' + process.env.appID).then(function(responseData) {
-    rtm.sendMessage(msg + responseData.status.titleCase(), channel);
-  });
+function setStatus(item, field, value, channel) {
+  return getItemID(item, field)
+    .then((item_id) => {
+      return podio.request('PUT', `/item/${item_id}/value/${field}`).then((res) => {
+        rtm.sendMessage('Item: ' + item + ', Field: ' + field + ', Value(s): ' + res, channel);
+      });
+    });
+
 }
 
 function authenticatePodio(callback, errorCallback) {
-  return podio.authenticateWithApp(process.env.appID, process.env.appToken, function(err) {
-    return podio.isAuthenticated().then(function() {
+  return podio.authenticateWithApp(process.env.appID, process.env.appToken, (err) => {
+    return podio.isAuthenticated().then(() => {
       callback();
-    }).catch(function(err) {
+    }).catch((err) => {
       errorCallback(err);
     });
   });
 }
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload if you want to cache it
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function(rtmStartData) {
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-  authenticatePodio(function() {
+  authenticatePodio(() => {
     podioAuthenticated = true;
-  }, function(err) {
+  }, (err) => {
     console.log(err);
   });
 });
 
 // // you need to wait for the client to fully connect before you can send messages
-rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
   rtm.sendMessage('Hello! Just letting you know that I\'m here if you need anything.', 'C46S9UAN5');
 });
 
-rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+rtm.on(RTM_EVENTS.MESSAGE, (message) => {
   // Looks for '@podio title get status'
   const msg = message.text.split(' ');
-  const channel = message.channel;
 
   if (podioAuthenticated && msg[0] === '@podio') {
+    const channel = message.channel;
+    const item = msg[1];
+    const action = msg[2]
+    const field = msg[3];
+    const value = msg[4];
     switch (true) {
-      case msg[2] === 'get':
-        let item = msg[1];
-        let field = msg[3];
-        getStatus(item, field, channel).catch(function(err) {
+      case action === 'get':
+        getStatus(item, field, channel).catch((err) => {
           console.log(err);
         });
         break;
-      case msg[2] === 'set':
-        //setStatus(msg[4], res, id);
+      case action === 'set':
+        getItemID(item,field);
+        setStatus(item, field, value, channel).catch((err) => {
+          console.log(err);
+        });
         break;
       default:
         rtm.sendMessage('Sorry, wrong command', channel);
