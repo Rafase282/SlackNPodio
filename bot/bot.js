783f58a55944d9b8e76aa0fb6033e1615bd641ce
Rafael J. Rodriguez <rafase282@gmail.com>
2017-04-08 13:14:32 -0400

    Better documentation on bot functions

--- bot/bot.js
+++ bot/bot.js
@@ -15,12 +15,16 @@ const podio = new Podio({
 });
 let podioAuthenticated = false;
 
-/* Function with the podio api call to get all items and filter by excat title
- * Note: Also providing a "property" for the filter would allows us to filter
- * by anything other than the title. Something useful for the new get/list.
- * However, this API calls required an excat match although it is not case sensitive.
- * I would say it is only good for getting specific things, not really useful for lists.
- */
+ /**
+  * Podio API call to filter items by excat titles.
+  *
+  * Note: Also providing a "property" for the filter would allows us to filter
+  * by anything other than the title. Something useful for the new get/list.
+  * However, this API calls required an excat match although it is not case sensitive.
+  * I would say it is only good for getting specific things, not really useful for lists.
+  * @param {String} name
+  * @return {Object}
+ **/
 const getPodioItem = exports.getPodioItem = (name) => {
   const data = {
     'sort_by': 'title',
@@ -36,48 +40,66 @@ const getPodioItem = exports.getPodioItem = (name) => {
   return podio.request('POST', '/item/app/17912486/filter/', data).then((res) => res.items[0]);
 }
 
-/* Main function for retrieving the link to the item.
- * Users must provide the item name. It depends on
- * getPodioItem to find the right item.
- */
+/**
+ * Provides message with the item's link.
+ * Users must provide the item name. It depends on getPodioItem to find the right item.
+ * Usage: @podio {name} url
+ * @param {String} name
+ * @return {String}
+ **/
 const getURL = exports.getURL = (name) => getPodioItem(name).then((item) =>
   `Item: ${name}, Item Link: ${bot.getURL(item)}`);
-/* Main function for retrieving field values
- * when you know the item's excat title, it depends on
+/**
+ * Retrieves field's value when you know the item's excat title, it depends on
  * getPodioItem to find the right item.
- */
-const getValue = exports.getValue = (item_name, field_name) => {
-  return getPodioItem(item_name).then((item) => {
-    let res = bot.filterFields(item.fields, field_name);
+ * Usage: @podio {item} get {name}
+ * @param {String} item
+ * @param {String} name
+ * @return {String}
+ **/
+const getValue = exports.getValue = (item, name) => {
+  return getPodioItem(item).then((item) => {
+    let res = bot.filterFields(item.fields, name);
     if (typeof res !== 'undefined') {
       //Returns either a number, string, or whole value.
       res = bot.checkValue(res.values[0].value);
     }
-      return `Item: ${item_name}, Field: ${field_name}, Value: ${res}`;
+      return `Item: ${item}, Field: ${name}, Value: ${res}`;
   });
 }
-// Sets status field to value Active or Inactive
-//Action: @podio Item's Name set Field's Name Value
-const setValue = exports.setValue = (item_name, field_name, field_value) => {
-  return getPodioItem(item_name).then((item) => {
-    // Check why we are getting the second one, this seems hardcoded.
+/**
+ * Writes a new field's value when you know the item's excat title, it depends on
+ * getPodioItem to find the right item.
+ * Usage: @podio {item} set {name} {value}
+ * @param {String} item
+ * @param {String} name
+ * @param {String} value
+ * @return {String}
+ **/
+const setValue = exports.setValue = (item, name, value) => {
+  return getPodioItem(item).then((item) => {
     const options = item.fields[1].config.settings.options;
     const item_id = bot.getItemID(item);
-    let fieldID = field_value;
+    let fieldID = value;
     let data = {};
-    if (field_name === 'Category' || field_name === 'category') {
-      fieldID = bot.getFieldValueID(options, field_value);
+    if (name === 'Category' || name === 'category') {
+      fieldID = bot.getFieldValueID(options, value);
       data = {
         'category': [fieldID]
       };
     }
-    data[field_name.toLowerCase()] = fieldID;
+    data[name.toLowerCase()] = fieldID;
     return podio.request('PUT', `/item/${item_id}/value/`, data).then((res) => {
-      return `Item: ${item_name}, Field: ${field_name}, Value set to: ${field_value}`;
+      return `Item: ${item}, Field: ${name}, Value set to: ${value}`;
     });
   });
 }
-
+/**
+ * Authenticates the bot with the podio api.
+ * @param {Function} callback
+ * @param {Function} errorCallback
+ * @return {Boolean}
+ **/
 function authenticatePodio(callback, errorCallback) {
   return podio.authenticateWithApp(process.env.appID, process.env.appToken, (err) => {
     errorCallback(err);
@@ -88,9 +110,13 @@ function authenticatePodio(callback, errorCallback) {
     });
   });
 }
-
-// The client will emit an RTM.AUTHENTICATED event on successful connection,
-// with the `rtm.start` payload if you want to cache it
+/**
+ * Event handler for when the bot is authenticated with Podio
+ * The client will emit an RTM.AUTHENTICATED event on successful connection,
+ * with the `rtm.start` payload if you want to cache it.
+ * @param {Stream} rtmStartData
+ * @return {Log}
+ **/
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name},` +
     `but not yet connected to a channel.`);
@@ -101,15 +127,23 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
   });
 });
 
-// you need to wait for the client to fully connect before you can send messages
+/**
+ * Event handler for when the bot connects to slack.
+ * You need to wait for the client to fully connect before you can send messages.
+ **/
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
   rtm.sendMessage(`Hello! Just letting you know that I'm here if you need anything.`, 'C46S9UAN5');
 });
-
+/**
+ * Event handler for when a messaged is posted on the slack channel.
+ * The message will be split to look for keywords containing the commands and data
+ * needed to excecute the user's desired actions.
+ * The logic that controls what gets called is here.
+ * @param {String} message
+ * @return {String}
+ **/
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
-  // Looks for '@podio title get status'
   const msg = message.text.split(' ');
-
   if (podioAuthenticated && msg[0] === '@podio') {
     const channel = message.channel;
     const item = msg[1];
