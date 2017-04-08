2ab4b2744bd6b3b7bc062eea8432ed1e35c80f9d
Rafael J. Rodriguez <rafase282@gmail.com>
2017-04-08 00:21:47 -0400

    Better organization, more functional programing.

--- bot.js
+++ bot.js
@@ -12,103 +12,92 @@ const podio = new Podio({
   clientId: process.env.clientId,
   clientSecret: process.env.clientSecret
 });
+const bot = require('./helper');
 let podioAuthenticated = false;
 
-// Function to filter array of fields by label or text
-function filterFields(fields, key) {
-  return fields.filter((field) => field.label === key || field.text === key);
-}
-
-// Function with the podio api call to get all items and filter by excat title
-function filterItems(item_name) {
+/* Function with the podio api call to get all items and filter by excat title
+ * Note: Also providing a "property" for the filter would allows us to filter
+ * by anything other than the title. Something useful for the new get/list.
+ * However, this API calls required an excat match although it is not case sensitive.
+ * I would say it is only good for getting specific things, not really useful for lists.
+ */
+const getPodioItem = (name) => {
   const data = {
     'sort_by': 'title',
     'sort_desc': true,
     'filters': {
-      title: item_name
+      title: name
     },
     'limit': 30,
     'offset': 0,
     'remember': false
   }
   // Returns Filtered Item Object
-  return podio.request('POST', '/item/app/17912486/filter/', data)
-    .then((res) => res.items);
+  return podio.request('POST', '/item/app/17912486/filter/', data).then((res) => res.items[0]);
 }
 
-// Gets item's ID
-function getItemID(items_arr) {
-  return items_arr[0].item_id;
-}
+/* Main function for retrieving the link to the item.
+ * Users must provide the item name. It depends on
+ * getPodioItem to find the right item.
+ */
+function getURL(item_name, channel) {
+  return getPodioItem(item_name).then((item) => {
+    const msg = bot.getURL(item);
+    rtm.sendMessage(msg, channel);
+  });
 
-// Gets field id
-function getFieldID(items_arr, field_name) {
-  return filterFields(items_arr[0].fields, field_name)[0].field_id;
 }
-
-// Gets field's value id
-function getFieldValueID(options_arr, field_value) {
-  return filterFields(options_arr, field_value)[0].id;
-}
-
-// function to get values
+/* Main function for retrieving field values
+ * when you know the item's excat title, it depends on
+ * getPodioItem to find the right item.
+ * TODO: Decouple more. Make a function to handle res,
+ * what should be the final response?
+ */
 function getStatus(item_name, field_name, channel) {
-  return filterItems(item_name).then((items) => {
-    let res = filterFields(items[0].fields, field_name)[0].values[0].value;
+  return getPodioItem(item_name).then((item) => {
+    let res = bot.filterFields(item.fields, field_name)[0].values[0].value;
     //Returns either a number, string, or whole value.
-    res =  parseInt(res, 10) || res.text || res;
-    rtm.sendMessage(
-      `Item: ${item_name}, Field: ${field_name}, Value: ${res}`,
-      channel
-    );
+    res = parseInt(res, 10) || res.text || (typeof res === 'object'? JSON.stringify(res) : res);
+    rtm.sendMessage(`Item: ${item_name}, Field: ${field_name}, Value: ${res}`, channel);
   });
 }
 
 // Sets status field to value Active or Inactive
 //Action: @podio Item's Name set Field's Name Value
 function setStatus(item_name, field_name, field_value, channel) {
-  return filterItems(item_name).then((item) => {
-    const options = item[0].fields[1].config.settings.options;
-    const item_id = getItemID(item);
+  return getPodioItem(item_name).then((item) => {
+    const options = item.fields[1].config.settings.options;
+    const item_id = bot.getItemID(item);
     let fieldID = field_value;
     let data = {};
     if (field_name === 'Category' || field_name === 'category') {
-      fieldID = getFieldValueID(options, field_value);
+      fieldID = bot.getFieldValueID(options, field_value);
       data = {
         'category': [fieldID]
       };
     }
     data[field_name.toLowerCase()] = fieldID;
-    return podio.request('PUT', `/item/${item_id}/value/`, data)
-      .then((res) => {
-        rtm.sendMessage(
-          `Item: ${item_name}, Field: ${field_name},` +
-          ` Value set to: ${field_value}`,
-          channel
-        );
-      });
+    return podio.request('PUT', `/item/${item_id}/value/`, data).then((res) => {
+      rtm.sendMessage(`Item: ${item_name}, Field: ${field_name},` + ` Value set to: ${field_value}`, channel);
+    });
   });
 }
 
 function authenticatePodio(callback, errorCallback) {
-  return podio.authenticateWithApp(process.env.appID, process.env.appToken,
-    (err) => {
+  return podio.authenticateWithApp(process.env.appID, process.env.appToken, (err) => {
+    errorCallback(err);
+    return podio.isAuthenticated().then(() => {
+      callback();
+    }).catch((err) => {
       errorCallback(err);
-      return podio.isAuthenticated().then(() => {
-        callback();
-      }).catch((err) => {
-        errorCallback(err);
-      });
     });
+  });
 }
 
 // The client will emit an RTM.AUTHENTICATED event on successful connection,
 // with the `rtm.start` payload if you want to cache it
 rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-  console.log(
-    `Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name},`
-    + `but not yet connected to a channel.`
-  );
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name},` + `but not yet connected to a channel.`);
   authenticatePodio(() => {
     podioAuthenticated = true;
   }, (err) => {
@@ -118,10 +107,7 @@ rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
 
 // you need to wait for the client to fully connect before you can send messages
 rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
-  rtm.sendMessage(
-    `Hello! Just letting you know that I'm here if you need anything.`,
-    'C46S9UAN5'
-  );
+  rtm.sendMessage(`Hello! Just letting you know that I'm here if you need anything.`, 'C46S9UAN5');
 });
 
 rtm.on(RTM_EVENTS.MESSAGE, (message) => {
@@ -144,6 +130,14 @@ rtm.on(RTM_EVENTS.MESSAGE, (message) => {
         setStatus(item, field, value, channel).catch((err) => {
           console.log(err);
         });
+        break;
+      case action === 'url':
+        getURL(item, channel).catch((err) => {
+          console.log(err);
+        });
+        break;
+      case item === 'help':
+        rtm.sendMessage(bot.showHelp(), channel);
         break;
       default:
         rtm.sendMessage('Sorry, wrong command', channel);
---
+++ helper.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Function to filter array of fields by label or text
+const filterFields = exports.filterFields = (fields, key) => {
+  return fields.filter((field) => field.label === key || field.text === key);
+}
+// Gets item's ID
+const getItemID = exports.getItemID = (items_arr) => items_arr.item_id;
+// Gets field id
+const getFieldID = exports.getFieldID = (items_arr, field_name) => {
+  return filterFields(items_arr.fields, field_name).field_id;
+}
+// Gets field's value id
+const getFieldValueID = exports.getFieldValueID = (options_arr, field_value) => {
+  return filterFields(options_arr, field_value).id;
+}
+// Show Help
+const showHelp = exports.showHelp = () => {
+  const help = `Show help message.`;
+  return help;
+}
+// Retrieves URL
+const getURL = exports.getURL = (itemObj) => `Item: ${itemObj.title}, Item Link: ${itemObj.link}`;
